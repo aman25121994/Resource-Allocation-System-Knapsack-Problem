32a844cd7996f625924e6d02866a5308bda84d8b
Amanuel Mengistu <136717053+aman25121994@users.noreply.github.com>
2025-01-14 21:57:29 +0300

    Update knapsack.cpp

--- knapsack.cpp
+++ knapsack.cpp
@@ -1,3 +1,22 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+
+void knapsack(int n, std::vector<float> &weight, std::vector<float> &profit, float capacity)
+{
+    std::vector<float> x(n, 0.0);
+    float tp = 0;
+    int i;
+    float u = capacity;
+
+    for (i = 0; i < n; i++)
+    {
+        if (weight[i] > u)
+            break;
+        else
+        {
+            x[i] = 1.0;
+            tp += profit[i];
+            u -= weight[i];
+        }
+    }
